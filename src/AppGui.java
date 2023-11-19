cae3e802e0d4f6f33bb7c12b3871eac9743bd8c7
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-11-19 10:16:54 -0500

    Created display

---
+++ src/AppGui.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class AppGui extends JFrame {
+   public AppGui(){
+       this.setTitle("Weather App");
+
+
+       setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+       setSize(500,500);
+
+       setLocationRelativeTo(null);
+
+       setLayout(null);
+
+       setResizable(false);
+   }
+}
---
+++ src/Launcher.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        //thread safe
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                AppGui app = new AppGui();
+                app.setVisible(true);
+            }
+        });
+
+    }
+}
