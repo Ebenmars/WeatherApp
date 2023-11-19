9777d9e396a4bc82e2672ff4e13d149fffcc4d05
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-11-19 13:05:49 -0500

    All The Icons Will Have Color

--- src/AppGui.java
+++ src/AppGui.java
@@ -1,4 +1,10 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class AppGui extends JFrame {
    public AppGui(){
@@ -19,10 +25,43 @@ public class AppGui extends JFrame {
    }
 
     private void addComponets() {
-       JTextField seachfield = new JTextField();
+       JTextField searchfield = new JTextField();
 
-       seachfield.setBounds(15,15,400,40);
+       searchfield.setBounds(15,15,400,40);
 
-       this.add(seachfield);
+        searchfield.setFont(new Font("SERIF",Font.ITALIC,16));
+
+        searchfield.setHorizontalAlignment(JTextField.CENTER);
+
+        searchfield.setBorder(new LineBorder(Color.BLACK,1,true));
+       this.add(searchfield);
+
+
+       JButton searchButton = new JButton(loadImage("/Users/ignite/WeatherApp/src/assets/search.png"));
+
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchButton.setBounds(425,15,47,40);
+        searchButton.setBorder(new LineBorder(Color.BLACK,1,true));
+        this.add(searchButton);
+
+
+        JLabel weatherConditionImage = new JLabel();
+    }
+
+    private ImageIcon loadImage(String path) {
+       try {
+           //reading path
+           BufferedImage image = ImageIO.read(new File(path));
+
+           //return image
+           return new ImageIcon(image);
+       }
+       catch(IOException e){
+           e.printStackTrace();
+       }
+
+        System.out.println("asset not found");
+
+       return null;
     }
 }
